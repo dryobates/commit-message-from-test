1dab96ee67ec3219e8ffe901d3b86f4bf3685b9e
dryobates <jakub.stolarski@gmail.com>
2019-08-22 20:40:58 +0200

    T shows usage when no default message provided

--- tests/test_commitmessagefromtest.py
+++ tests/test_commitmessagefromtest.py
@@ -1,4 +1,13 @@
-# - exits with 1 and prints usage, when no default message
+from click.testing import CliRunner
+from commitmessagefromtest import main
+
+
+def test_shows_usage_when_no_default_message_provided():
+    runner = CliRunner()
+    result = runner.invoke(main, [])
+    assert result.exit_code == 1
+    assert result.output.startswith('Usage:')
+
 # - prints on stdout default message when no test found in git diff
 # - prints on stdout message based on test name when one test found in git diff
 # - prints on stdout message based on first found test name when many tests found in git diff
