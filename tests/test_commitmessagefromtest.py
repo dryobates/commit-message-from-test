4aaa8724ce891553ef5c26660436337f4ab52e18
dryobates <jakub.stolarski@gmail.com>
2019-08-22 20:33:38 +0200

    Initial tests.

---
+++ tests/test_commitmessagefromtest.py
@@ -0,0 +1,5 @@
+# - exits with 1 and prints usage, when no default message
+# - prints on stdout default message when no test found in git diff
+# - prints on stdout message based on test name when one test found in git diff
+# - prints on stdout message based on first found test name when many tests found in git diff
+# - runs git diff for given directory/file when path is given as option
