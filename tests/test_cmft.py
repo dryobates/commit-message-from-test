c9de055f97d0bc8c02ea6979f99320447ae6e696
dryobates <jakub.stolarski@gmail.com>
2019-08-27 21:44:32 +0200

    T prints on stdout default message when no tests found in git diff

--- tests/test_cmft.py
+++ tests/test_cmft.py
@@ -1,3 +1,4 @@
+from subprocess import run
 from click.testing import CliRunner
 
 from cmft.cli import main
@@ -19,7 +20,18 @@ def test_prints_on_stdout_default_message_when_could_not_run_git_diff():
         assert f"{default_message}\n" == result.output
 
 
-# - prints on stdout default message when no test found in git diff
+def test_prints_on_stdout_default_message_when_no_tests_found_in_git_diff():
+    default_message = "some message"
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        run("git init .", shell=True)
+        run("touch test_example.py", shell=True)
+        run("git add test_example.py", shell=True)
+        result = runner.invoke(main, [default_message])
+        assert result.exit_code == 0
+        assert f"{default_message}\n" == result.output
+
+
 # - prints on stdout message based on test name when one test found in git diff
 # - prints on stdout message based on first found test name when many tests found in git diff
 # - runs git diff for given directory/file when path is given as option
