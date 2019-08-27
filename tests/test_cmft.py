301586a15e9e3cae2a48be2c942ad4458e480726
dryobates <jakub.stolarski@gmail.com>
2019-08-27 21:37:10 +0200

    T prints on stdout default message when not in git repo

--- tests/test_cmft.py
+++ tests/test_cmft.py
@@ -7,9 +7,25 @@ def test_shows_usage_when_no_default_message_provided():
     runner = CliRunner()
     result = runner.invoke(main, [])
     assert result.exit_code != 0
-    assert result.output.startswith('Usage:')
+    assert result.output.startswith("Usage:")
 
+
+def test_prints_on_stdout_default_message_when_not_in_git_repo():
+    default_message = "some message"
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(main, [default_message])
+        assert result.exit_code == 0
+        assert "f{default_message}\n" == result.output
+
+
+# - prints on stderr error when could not run git diff
 # - prints on stdout default message when no test found in git diff
 # - prints on stdout message based on test name when one test found in git diff
 # - prints on stdout message based on first found test name when many tests found in git diff
 # - runs git diff for given directory/file when path is given as option
+# - test is method
+# - test has arguments
+# - test is commented
+# - test contains "test" in name
+# - test contains "def" in name
