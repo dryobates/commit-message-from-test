b84d119a4a4be50ac0237d6a62c40fbaab6bce3d
dryobates <jakub.stolarski@gmail.com>
2019-08-27 21:38:18 +0200

    B prints on stdout default message when not in git repo

--- cmft/cli.py
+++ cmft/cli.py
@@ -1,10 +1,15 @@
+from subprocess import CalledProcessError, run
+
 import click
 
 
 @click.command()
 @click.argument("message")
 def main(message):
-    pass
+    try:
+        run("git diff", shell=True, check=True)
+    except CalledProcessError:
+        click.echo(message)
 
 
 if __name__ == "__main__":
