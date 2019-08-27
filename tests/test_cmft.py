a0b40462d560e9ad8c1bfdf45c2949ea6e1bec05
dryobates <jakub.stolarski@gmail.com>
2019-08-27 21:53:36 +0200

    T prints on stdout message based on test name when one test found in git diff

--- tests/test_cmft.py
+++ tests/test_cmft.py
@@ -36,7 +36,19 @@ def test_prints_on_stdout_default_message_when_no_tests_found_in_git_diff(runner
         assert f"{default_message}\n" == result.output
 
 
-# - prints on stdout message based on test name when one test found in git diff
+def test_prints_on_stdout_message_based_on_test_name_when_one_test_found_in_git_diff(runner):
+    default_message = "some message"
+    with runner.isolated_filesystem():
+        run("git init .", shell=True)
+        with open("test_example.py", "w") as test_file:
+            test_file.write("+def test_name")
+        run("git add test_example.py", shell=True)
+
+        result = runner.invoke(main, [default_message])
+
+        assert result.exit_code == 0
+        assert "name" == result.output
+
 # - prints on stdout message based on first found test name when many tests found in git diff
 # - runs git diff for given directory/file when path is given as option
 # - test is method
