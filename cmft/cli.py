a3cf9d0058ea644c27c2e4596bd891f46e626ded
dryobates <jakub.stolarski@gmail.com>
2019-08-30 22:10:21 +0200

    S structure to input-filter-output

--- cmft/cli.py
+++ cmft/cli.py
@@ -1,19 +1,15 @@
-import re
 from subprocess import CalledProcessError, run
 
 import click
 
-TEST_RE = re.compile(r"^\+\s*def test(.*)\(", re.MULTILINE)
-UNDERLINES_RE = re.compile("_+")
-FIRST_CAP_RE = re.compile(r"(.)([A-Z][a-z]+)")
-ALL_CAP_RE = re.compile("([a-z0-9])([A-Z])")
+from cmft.extract_message import extract_message
 
 
 @click.command()
 @click.argument("message")
 def main(message):
     output = _get_diff_output()
-    message = _extract_message(output, message)
+    message = extract_message(output, message)
     click.echo(message, nl=False)
 
 
@@ -28,23 +24,6 @@ def _get_diff_output():
         ).stdout
     except CalledProcessError:
         return ""
-
-
-def _extract_message(output, default_message):
-    match = TEST_RE.search(output)
-    if match:
-        return snake_to_words(camel_to_snake(match.group(1)))
-    else:
-        return default_message
-
-
-def camel_to_snake(message):
-    message = FIRST_CAP_RE.sub(r"\1_\2", message)
-    return ALL_CAP_RE.sub(r"\1_\2", message).lower()
-
-
-def snake_to_words(message):
-    return UNDERLINES_RE.sub(" ", message).strip()
 
 
 if __name__ == "__main__":
---
+++ cmft/extract_message.py
@@ -0,0 +1,23 @@
+import re
+
+TEST_RE = re.compile(r"^\+\s*def test(.*)\(", re.MULTILINE)
+UNDERLINES_RE = re.compile("_+")
+FIRST_CAP_RE = re.compile(r"(.)([A-Z][a-z]+)")
+ALL_CAP_RE = re.compile("([a-z0-9])([A-Z])")
+
+
+def extract_message(output, default_message):
+    match = TEST_RE.search(output)
+    if match:
+        return snake_to_words(camel_to_snake(match.group(1)))
+    else:
+        return default_message
+
+
+def camel_to_snake(message):
+    message = FIRST_CAP_RE.sub(r"\1_\2", message)
+    return ALL_CAP_RE.sub(r"\1_\2", message).lower()
+
+
+def snake_to_words(message):
+    return UNDERLINES_RE.sub(" ", message).strip()
