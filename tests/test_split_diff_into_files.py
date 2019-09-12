022dcf24fbc7c7389d584a53369738ab05c5ca75
dryobates <jakub.stolarski@gmail.com>
2019-09-12 22:14:20 +0200

    T returns iterable with one element when diff with one file

--- tests/test_split_diff_into_files.py
+++ tests/test_split_diff_into_files.py
@@ -1,7 +1,6 @@
-# returns iterator over file diffs
+# returns iterator over file diffs with two items
 # recognizes language from diff
 # ignores file diff when not supported language
-# returns empty iterator when empty diff
 
 from cmft.split_diff_into_files import split_diff_into_files
 
@@ -20,3 +19,19 @@ def test_returns_empty_iterator_when_no_empty_diff():
     file_diffs = list(split_diff_into_files(diff))
 
     assert file_diffs == []
+
+
+def test_returns_iterable_with_one_element_when_diff_with_one_file():
+    diff = """\
+diff --git a/file.txt b/file.txt
+new file mode 100644
+index 0000000..0d6170b
+--- /dev/null
++++ b/file.txt
+@@ -0,0 +1 @@
++# test
+    """
+
+    file_diffs = list(split_diff_into_files(diff))
+
+    assert len(file_diffs)
