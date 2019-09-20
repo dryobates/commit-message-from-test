a2033257739b37df60d295b3c915c6094cbd0eec
dryobates <jakub.stolarski@gmail.com>
2019-09-20 21:32:52 +0200

    S Recognizes language from diff

--- cmft/split_diff_into_files.py
+++ cmft/split_diff_into_files.py
@@ -1,16 +1,16 @@
 import re
 
+LANG_EXT = re.compile(r"^\+\+\+ .*\.(?P<ext>.*)$", re.MULTILINE)
+KNOWN_FILES = {"py"}
+
 
 def split_diff_into_files(diff):
     chunks = ("\n" + diff).split("\ndiff ")
     return chunks[1:]
 
 
-KNOWN_FILES = {"py"}
-
-
 def recognize_lang(diff):
-    match = re.search(r"^\+\+\+ .*\.(?P<ext>.*)$", diff, re.MULTILINE)
+    match = LANG_EXT.search(diff)
     if match:
         ext = match.group("ext")
         if ext in KNOWN_FILES:
