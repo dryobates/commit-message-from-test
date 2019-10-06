102614f210302fc937590960ee7b210c0f33377d
dryobates <jakub.stolarski@gmail.com>
2019-10-06 22:46:24 +0200

    B returns message based on test name declared with function keyword

--- cmft/extract_from_shell.py
+++ cmft/extract_from_shell.py
@@ -1,8 +1,8 @@
 import re
 
-SHELL_TEST_RE = re.compile(r"^\+\s*test(.*)\(", re.MULTILINE)
+SHELL_TEST_RE = re.compile(r"^\+\s*(?:test(.*)\(|function\s*test([^\s]*))", re.MULTILINE)
 
 
 def extract_messages_from_shell_file_diff(diff):
     matches = SHELL_TEST_RE.findall(diff)
-    return (match for match in matches)
+    return (m1 or m2 for m1, m2 in matches)
